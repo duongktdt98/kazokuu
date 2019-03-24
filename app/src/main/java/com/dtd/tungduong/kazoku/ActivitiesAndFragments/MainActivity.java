2b5826d6a0def0ae25c2ec02f5774b16b2629c39
duong <dtduong007>
2019-03-24 18:13:48 +0700

    pkpk

---
+++ app/src/main/java/com/dtd/tungduong/kazoku/ActivitiesAndFragments/MainActivity.java
@@ -0,0 +1,97 @@
+package com.dtd.tungduong.kazoku.ActivitiesAndFragments;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.view.Window;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dtd.tungduong.kazoku.Constants.PreferenceClass;
+import com.dtd.tungduong.kazoku.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private long mBackPressed;
+    public static SharedPreferences sPre;
+    private static final int TIME_INTERVAL = 2000; // # milliseconds, desired time passed between two back presses.
+    private TextView mTextMessage;
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = getSupportActionBar();
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(new SearchFragment());
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_search:
+                    fragment = new SearchFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_chat:
+                    fragment = new ChatFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_location:
+                    fragment = new LocationFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_user:
+                    fragment = new UserFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (mBackPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+            super.onBackPressed();
+            MainActivity.this.finish();
+            return;
+        } else {
+            Toast.makeText(getBaseContext(), "Nhấn lần nữa để thoát ứng dụng", Toast.LENGTH_SHORT).show();
+
+            mBackPressed = System.currentTimeMillis();
+
+        }
+
+    }
+
+
+}
