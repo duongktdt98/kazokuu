53dd436fac6cf3cbdf1c240fb96cc3fd4d32fd55
duong <dtduong007>
2019-04-07 16:25:19 +0700

    Tạo nút back khi đăng ký

--- app/src/main/java/com/dtd/tungduong/kazoku/ActivitiesAndFragments/LoginFragment.java
+++ app/src/main/java/com/dtd/tungduong/kazoku/ActivitiesAndFragments/LoginFragment.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -65,9 +66,14 @@ public class LoginFragment extends Fragment {
 
             }
         });
+        final FragmentManager fragmentManager = getFragmentManager();
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         txsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SignUp signUpfragment = new SignUp();
+                fragmentTransaction.replace(R.id.frame_login, signUpfragment);
+                fragmentTransaction.commit();
             }
         });
 
--- app/src/main/java/com/dtd/tungduong/kazoku/ActivitiesAndFragments/SignUp.java
+++ app/src/main/java/com/dtd/tungduong/kazoku/ActivitiesAndFragments/SignUp.java
@@ -4,29 +4,43 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.dtd.tungduong.kazoku.R;
 
 
 public class SignUp extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ImageView iconback;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        AnhXa(view);
+        final FragmentManager fragmentManager = getFragmentManager();
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        iconback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginFragment loginFragment = new LoginFragment();
+                fragmentTransaction.replace(R.id.frame_login, loginFragment);
+                fragmentTransaction.commit();
+            }
+        });
+        return view;
     }
 
+    private void  AnhXa(View view){
+        iconback  = (ImageView) view.findViewById(R.id.back_icon);
+
+    }
 
 }
