9c319c6d61d8d05ed3144a7c7ca50780d926ec61
duong <dtduong007>
2019-04-22 18:17:34 +0700

    fix lỗi + tạo processLoad

--- app/src/main/java/com/dtd/tungduong/kazoku/ActivitiesAndFragments/HomeAdapter.java
+++ app/src/main/java/com/dtd/tungduong/kazoku/ActivitiesAndFragments/HomeAdapter.java
@@ -1,6 +1,11 @@
 package com.dtd.tungduong.kazoku.ActivitiesAndFragments;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +13,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.squareup.picasso.Transformation;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.dtd.tungduong.kazoku.R;
@@ -63,7 +68,40 @@ public class HomeAdapter extends BaseAdapter {
 //                .into(imageView);
         return view;
     }
+    public class CircleTransform implements Transformation {
+        @Override
+        public Bitmap transform(Bitmap source) {
+            int size = Math.min(source.getWidth(), source.getHeight());
 
+            int x = (source.getWidth() - size) / 2;
+            int y = (source.getHeight() - size) / 2;
+
+            Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+            if (squaredBitmap != source) {
+                source.recycle();
+            }
+
+            Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+
+            Canvas canvas = new Canvas(bitmap);
+            Paint paint = new Paint();
+            BitmapShader shader = new BitmapShader(squaredBitmap,
+                    BitmapShader.TileMode.CLAMP,  Shader.TileMode.CLAMP);
+            paint.setShader(shader);
+            paint.setAntiAlias(true);
+
+            float r = size / 2f;
+            canvas.drawCircle(r, r, r, paint);
+
+            squaredBitmap.recycle();
+            return bitmap;
+        }
+
+        @Override
+        public String key() {
+            return "circle";
+        }
+    }
     public void getView(ArrayList<HinhAnh> arrayImage, Context context) {
 
     }
