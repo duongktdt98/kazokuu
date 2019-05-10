56c9efd27291c3e7187c9ad725a2da73a04dca46
duong <dtduong007>
2019-05-10 17:08:38 +0700

    Tạo sniper chọn thành phố

--- app/src/main/java/com/dtd/tungduong/kazoku/ActivitiesAndFragments/AddRoom1.java
+++ app/src/main/java/com/dtd/tungduong/kazoku/ActivitiesAndFragments/AddRoom1.java
@@ -8,14 +8,21 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dtd.tungduong.kazoku.R;
 
+import java.util.ArrayList;
+
 public class AddRoom1 extends Fragment {
     TextView txt_add_room2;
-
+    Spinner spinnercity;
+    // EditText
 
     @Override
 
@@ -28,6 +35,7 @@ public class AddRoom1 extends Fragment {
         txt_add_room2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 final FragmentManager fragmentManager = getFragmentManager();
                 final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 AddRoomFragment2 add2 = new AddRoomFragment2();
@@ -36,11 +44,44 @@ public class AddRoom1 extends Fragment {
                 fragmentTransaction.commit();
             }
         });
+
+        final ArrayList<String> arrayCity = new ArrayList<String>();
+        arrayCity.add("Hà Nội");
+        arrayCity.add("Hải Phòng");
+        arrayCity.add("Đà Nẵng");
+        arrayCity.add("Huế");
+        arrayCity.add("Nghệ An");
+        arrayCity.add("Hà Tĩnh");
+        arrayCity.add("Bắc Ninh");
+        arrayCity.add("Hà Nội");
+        arrayCity.add("Hải Phòng");
+        arrayCity.add("Đà Nẵng");
+        arrayCity.add("Huế");
+        arrayCity.add("Nghệ An");
+        arrayCity.add("Hà Tĩnh");
+        arrayCity.add("Bắc Ninh");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item, arrayCity);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnercity.setAdapter(arrayAdapter);
+
+        spinnercity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(),    arrayCity.get(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         return view;
     }
 
     private void Anhxa(View view) {
         txt_add_room2 = (TextView) view.findViewById(R.id.add_form2);
+        spinnercity = (Spinner) view.findViewById(R.id.spinnercity);
 
     }
 
